19bf09cd9795544e89f804220f76567f7ef862ef
Annick <annick.hauregard@poe.aelion.biz>
2023-03-14 10:23:05 +0100

    add list, interface, models,service and correction bug

--- src/app/dashboard/components/tile/tile.component.ts
+++ src/app/dashboard/components/tile/tile.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
-  styleUrls: ['./tile.component.scss']
+  styleUrls: ['./tile.component.scss'],
 })
 export class TileComponent implements OnInit {
+  @Input() public tileInfo: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
