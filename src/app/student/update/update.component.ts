d089d34ecd85e2664eeb1ebbf213203d15799c44
Annick <annick.hauregard@poe.aelion.biz>
2023-03-21 13:52:08 +0100

    Add private _route, and view snapshot with console.log

--- src/app/student/update/update.component.ts
+++ src/app/student/update/update.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
-  styleUrls: ['./update.component.scss']
+  styleUrls: ['./update.component.scss'],
 })
 export class UpdateComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    console.log(this._route.snapshot.paramMap.get('id'));
   }
-
 }
