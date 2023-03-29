a4941bde69ba904c922dc70a3b13125dde8c9df3
Annick <annick.hauregard@poe.aelion.biz>
2023-03-29 19:21:44 +0200

    prepare update

--- src/app/course/dialogs/course-form/course-form.component.ts
+++ src/app/course/dialogs/course-form/course-form.component.ts
@@ -1,15 +1,40 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Route } from '@angular/router';
+import { CourseModel } from '../../models/course-model';
+import { CourseService } from '../../services/course.service';
+import { CourseType } from '../../types/course-type';
+import { CourseAddComponent } from '../course-add/course-add.component';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
+  public form: FormGroup = new FormGroup({});
+  public course: CourseModel | null = null;
+  constructor(
+    private _course: CourseService,
+    private _courses: CourseAddComponent,
+    private _route: ActivatedRoute
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  get c(): { [key: string]: AbstractControl } {
+    return this._courses.c;
   }
-
+  onSubmit(): void {
+    this.course!.title = this.c['title'].value;
+    this.course!.objective = this.c['objective'].value;
+    this._course.update(this.course!).subscribe({
+      next: (response: HttpResponse<any>) => {
+        console.log(`Course was updated ${response.status}`);
+      },
+      error: (error: any) => {
+        console.log(JSON.stringify(error));
+      },
+    });
+  }
 }
--- src/app/course/services/course.service.ts
+++ src/app/course/services/course.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { IStudent } from 'src/app/student/interfaces/i-student';
@@ -6,10 +6,9 @@ import { StudentModel } from 'src/app/student/models/student-model';
 import { CourseModel } from '../models/course-model';
 import { CourseType } from '../types/course-type';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { map, take, tap } from 'rxjs';
 import { environment } from './../../../environments/environment';
 import { CourseListType } from '../types/course-list-type';
-import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -27,10 +26,25 @@ export class CourseService {
   public findAll(): Observable<CourseListType[]> {
     return this._httpClient.get<CourseListType[]>(this.endpoint);
   }
-
+  public findOne(id: number): Observable<CourseListType> {
+    return this._httpClient.get<any>(this.endpoint + '/' + id).pipe(
+      tap((response: any) => {
+        //permet d'intercepté les réponses de l'étape juste avant (voir ce qu'il contient)
+        console.log(JSON.stringify(response));
+      }),
+      take(1),
+      map((course: any) => course)
+    );
+  }
   public add(course: CourseModel): Observable<any> {
     return this._httpClient
       .post<CourseModel>(this.endpoint, course)
       .pipe(take(1)); // ?? aligne les element 1 a 1 et take en prend que 1
+  }
+
+  public update(course: CourseModel): Observable<HttpResponse<any>> {
+    return this._httpClient.put<CourseModel>(this.endpoint, course, {
+      observe: 'response',
+    });
   }
 }
